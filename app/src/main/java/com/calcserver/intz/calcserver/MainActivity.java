453920d6ea6a4fdc88c4124fa5ceb5224f7ee871
Indrek Nurja <intz111@gmail.com>
2016-03-12 20:35:37 +0200

    Initial commit

---
+++ app/src/main/java/com/calcserver/intz/calcserver/MainActivity.java
@@ -0,0 +1,13 @@
+package com.calcserver.intz.calcserver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
