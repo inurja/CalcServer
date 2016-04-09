cb14d0a55ee2073f32973654a70ecf916f5a6d01
Indrek Nurja <intz111@gmail.com>
2016-04-09 13:26:59 +0300

    ---

--- app/src/main/java/com/calcserver/intz/calcserver/MainActivity.java
+++ app/src/main/java/com/calcserver/intz/calcserver/MainActivity.java
@@ -1,13 +1,48 @@
 package com.calcserver.intz.calcserver;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BroadcastReceiver {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void onReceive(Context context, Intent intent) {
+        int state=intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE, -1);
+        String msg=null;
+
+        switch (state) {
+            case WifiManager.WIFI_STATE_DISABLED:
+                msg="is disabled";
+                break;
+
+            case WifiManager.WIFI_STATE_DISABLING:
+                msg="is disabling";
+                break;
+
+            case WifiManager.WIFI_STATE_ENABLED:
+                msg="is enabled";
+                break;
+
+            case WifiManager.WIFI_STATE_ENABLING:
+                msg="is enabling";
+                break;
+
+            case WifiManager.WIFI_STATE_UNKNOWN :
+                msg="has an error";
+                break;
+
+            default:
+                msg="is acting strangely";
+                break;
+        }
+
+        if (msg!=null) {
+            Log.d("OnWiFiChanged", "WiFi " + msg);
+        }
     }
 }
